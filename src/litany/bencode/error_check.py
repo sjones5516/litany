9c80a2299a14c33caad7dea4929453668c3eb5e8
Samuel Jones <sjones5516@floridapoly.edu>
2026-03-23 08:50:28 -0400

    impl check has leading zero

--- src/litany/bencode/error_check.py
+++ src/litany/bencode/error_check.py
@@ -7,3 +7,22 @@ def _check_missing_terminator(data: bytes):
     """
     if b"e" not in data:
         raise ValueError("Missing 'e' terminator")
+
+
+def _check_has_leading_zero(data: bytes):
+    """
+    Ensures data does not have leading zero.
+    i<base10 integer>e format expected.
+    :param data: Data to check
+    :type data: bytes
+    :raises ValueError: Has a leading zero
+    """
+    n = len(data)
+    # Case 1: i-0...e
+    case_1 = n > 4 and data[1:3] == b"-0"
+
+    # Case 2: i0...e;
+    case_2 = n > 3 and data[1:2] == b"0"
+
+    if case_1 or case_2:
+        raise ValueError("Has a leading zero")
--- tests/test_bencode/test_error.py
+++ tests/test_bencode/test_error.py
@@ -1,6 +1,9 @@
 import unittest
 
-from src.litany.bencode.error_check import _check_missing_terminator
+from src.litany.bencode.error_check import (
+    _check_missing_terminator,
+    _check_has_leading_zero,
+)
 
 
 class TestCheckMissingTerminator(unittest.TestCase):
@@ -11,3 +14,17 @@ class TestCheckMissingTerminator(unittest.TestCase):
     def test_fails(self):
         data = b"i23"
         self.assertRaises(ValueError, _check_missing_terminator, data)
+
+
+class TestCheckHasLeadingZero(unittest.TestCase):
+    def test_succeeds(self):
+        data = b"i2e"
+        _check_has_leading_zero(data)
+
+    def test_fails_case_1(self):
+        data = b"i-02e"
+        self.assertRaises(ValueError, _check_has_leading_zero, data)
+
+    def test_fails_case_2(self):
+        data = b"i02e"
+        self.assertRaises(ValueError, _check_has_leading_zero, data)
