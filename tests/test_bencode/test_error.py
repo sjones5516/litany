d21a949aa9ef6dddcff7dffb32372112dc25f558
Samuel Jones <sjones.nova@gmail.com>
2026-03-24 16:26:17 -0400

    Removed unnessary tests from test_error

--- tests/test_bencode/test_error.py
+++ tests/test_bencode/test_error.py
@@ -1,25 +1,10 @@
 import unittest
 
 from src.litany.bencode.error_check import (
-    _check_missing_terminator,
     _check_has_leading_zero,
-    _check_is_negative_zero,
     _check_length_not_followed_by_colon,
-    _check_negative_length,
     _check_unexpected_eof_before_completing_string,
 )
-
-
-class TestCheckMissingTerminator(unittest.TestCase):
-    def test_succeeds(self):
-        data = b"i23e"
-        expected = 3
-        actual = _check_missing_terminator(data)
-        self.assertEqual(expected, actual)
-
-    def test_fails(self):
-        data = b"i23"
-        self.assertRaises(ValueError, _check_missing_terminator, data)
 
 
 class TestCheckHasLeadingZero(unittest.TestCase):
@@ -36,16 +21,6 @@ class TestCheckHasLeadingZero(unittest.TestCase):
         self.assertRaises(ValueError, _check_has_leading_zero, data)
 
 
-class TestCheckIsNegativeZero(unittest.TestCase):
-    def test_succeeds(self):
-        data = b"i0e"
-        _check_is_negative_zero(data)
-
-    def test_fails(self):
-        data = b"i-0e"
-        self.assertRaises(ValueError, _check_is_negative_zero, data)
-
-
 class TestLengthNotFollowedByColon(unittest.TestCase):
     def test_succeeds(self):
         data = b"1:e"
@@ -58,16 +33,6 @@ class TestLengthNotFollowedByColon(unittest.TestCase):
     def test_colon_after_char(self):
         data = b"1a:e"
         self.assertRaises(ValueError, _check_length_not_followed_by_colon, data)
-
-
-class TestCheckNegativeLength(unittest.TestCase):
-    def test_succeeds(self):
-        data = b"1:a"
-        _check_length_not_followed_by_colon(data)
-
-    def test_fails(self):
-        data = b"-1:a"
-        self.assertRaises(ValueError, _check_negative_length, data)
 
 
 class TestUnexpectedEOFBeforeCompletingString(unittest.TestCase):
